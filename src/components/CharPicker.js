ff5275f09fb6ada2d8ca981a64ea3d4780528928
Paul Vidler <45616638+PaulVidler@users.noreply.github.com>
2021-11-13 11:10:34 +1000

    Work in progress

--- src/components/CharPicker.js
+++ src/components/CharPicker.js
@@ -1,11 +1,15 @@
-import React, { Component } from 'react';
+import React, { useState } from 'react';
 
 import './CharPicker.css';
 
-class CharPicker extends Component {
+const CharPicker = (props) => {
+
+  const [loadedChars,setLoadedChars] = useState([]);
+  const [isLoading, setIsLoading] = useState(false);
+
   state = { characters: [], isLoading: false };
 
-  componentDidMount() {
+  useEffect() {
     this.setState({ isLoading: true });
     fetch('https://swapi.co/api/people')
       .then(response => {
@@ -21,29 +25,29 @@ class CharPicker extends Component {
             name: char.name,
             id: index + 1
           })),
-          isLoading: false
+          
         });
+        setIsLoading(false);
       })
       .catch(err => {
         console.log(err);
       });
   }
 
-  render() {
     let content = <p>Loading characters...</p>;
 
     if (
-      !this.state.isLoading &&
-      this.state.characters &&
-      this.state.characters.length > 0
+      !isLoading &&
+      loadedChars &&
+      loadedChars.length > 0
     ) {
       content = (
         <select
-          onChange={this.props.onCharSelect}
-          value={this.props.selectedChar}
-          className={this.props.side}
+          onChange={props.onCharSelect}
+          value={props.selectedChar}
+          className={props.side}
         >
-          {this.state.characters.map(char => (
+          {loadedChars.map(char => (
             <option key={char.id} value={char.id}>
               {char.name}
             </option>
@@ -51,13 +55,12 @@ class CharPicker extends Component {
         </select>
       );
     } else if (
-      !this.state.isLoading &&
-      (!this.state.characters || this.state.characters.length === 0)
+      !isLoading &&
+      (!loadedChars || loadedChars.length === 0)
     ) {
       content = <p>Could not fetch any data.</p>;
     }
     return content;
-  }
 }
 
 export default CharPicker;
